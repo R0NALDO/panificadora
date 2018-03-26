5b15c6d8dcbfc2e5c1ed71b231ec5f5e0ab9f240
Ronaldo Pereira <ox99709179@gmail.com>
2018-03-26 15:46:28 -0300

    testando persistência

--- src/com/up/controller/Main.java
+++ src/com/up/controller/Main.java
@@ -2,9 +2,36 @@ package com.up.controller;
 
 import java.util.Scanner;
 
-public class Principal { //utilizando hibernate
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.up.model.Cliente;
+
+public class Main { //utilizando hibernate
 
 	public static void main(String[] args) {
+		
+		Cliente fulano = new Cliente();
+		fulano.setId(1);
+		fulano.setNome("ana");
+		fulano.setEmail("ana@gmail.com");
+			//cria a fabrica de managers, e depois um manager
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("erp"); //a classe persistence representa o arquivo META-INF/persistence.xml, nesse arquivo está definindo o nome da unidade de persistencia
+		EntityManager em = emf.createEntityManager();
+			//salva as alteracoes na base
+		em.getTransaction().begin();
+		em.persist(fulano);
+		em.getTransaction().commit();
+			//fecha
+		em.close();
+		emf.close();
+		
+		
+		
+	}
+
+	private static void Menu() {
 		String nome = "";
 		int idade = 0;
 		String op = "";
